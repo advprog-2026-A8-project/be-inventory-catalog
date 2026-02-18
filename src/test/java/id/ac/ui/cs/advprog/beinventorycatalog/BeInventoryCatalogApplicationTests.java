ef9e80bb2c35837bfc6e39f268d432e5d6fd25c5
Jaysen Lestari <lestarijaysen@gmail.com>
2026-02-18 21:49:59 +0700

    init springboot project

---
+++ src/test/java/id/ac/ui/cs/advprog/beinventorycatalog/BeInventoryCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.beinventorycatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeInventoryCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
