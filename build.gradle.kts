a7083c8df393ad3dba3f18a22c4e7c8c50ce5402
Aryandana Pascua Patiung <nandapascua@Aryandanas-MacBook-Pro.local>
2026-03-04 00:12:41 +0700

    test: fix missing import for ActiveProfiles

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
+    testImplementation("com.h2database:h2")
 }
 
 tasks.withType<Test> {
